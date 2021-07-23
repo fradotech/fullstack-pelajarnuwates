4f7b0745d6f149f45728f0b1920b05f9320faf47
fradojr <fradoforumm@gmail.com>
2021-07-23 17:34:55 +0700

    try

--- app.js
+++ app.js
@@ -12,76 +12,83 @@ const Admin = require('./model/admin')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.set('view engine', 'ejs')
-// app.use(expressLayouts)
-// app.use(express.static('public'))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(cookieParser())
-// app.use(
-//     session({
-//         cookie: { maxAge: 6000 },
-//         secret: 'secret',
-//         resave: true,
-//         saveUninitialized: true,
-//     })
-// )
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(
+    session({
+        cookie: { maxAge: 6000 },
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+)
 
-// app.post('/login', (req, res) => {
-//     let email = req.body.email
-//     let password = req.body.password
+app.post('/login', (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
 
-//     const getUser = Admin.findOne({ email: email })
-//         .then(getUser => {
-//             if (getUser) {
-//                 if (password === getUser.password) {
-//                     token = jwt.sign({ email: getUser.email }, 'fradotech20012021.01082000.20052003', { expiresIn: '60m' })
+    const getUser = Admin.findOne({ email: email })
+        .then(getUser => {
+            if (getUser) {
+                if (password === getUser.password) {
+                    token = jwt.sign({ email: getUser.email }, 'fradotech20012021.01082000.20052003', { expiresIn: '60m' })
 
-//                     res.cookie('token', token)
-//                     res.cookie('admin', getUser)
+                    res.cookie('token', token)
+                    res.cookie('admin', getUser)
 
-//                     res.redirect('/')
+                    res.redirect('/')
 
-//                 } else {
-//                     res.render('login', {
-//                         layout: 'layouts/main-layout',
-//                         title: 'Pelajar NU Wates',
-//                         message: 'Password salah! Coba lagi!',
-//                         messageClass: 'alert-danger',
-//                         user: null
-//                     })
-//                 }
-//             } else {
-//                 res.render('login', {
-//                     layout: 'layouts/main-layout',
-//                     title: 'Pelajar NU Wates',
-//                     message: 'Email salah! Coba lagi!',
-//                     messageClass: 'alert-danger',
-//                     user: null
-//                 })
-//             }
-//         })
-// })
+                } else {
+                    res.render('login', {
+                        layout: 'layouts/main-layout',
+                        title: 'Pelajar NU Wates',
+                        message: 'Password salah! Coba lagi!',
+                        messageClass: 'alert-danger',
+                        user: null
+                    })
+                }
+            } else {
+                res.render('login', {
+                    layout: 'layouts/main-layout',
+                    title: 'Pelajar NU Wates',
+                    message: 'Email salah! Coba lagi!',
+                    messageClass: 'alert-danger',
+                    user: null
+                })
+            }
+        })
+})
 
-// //User
+//User
 
-// app.use(async (req, res, next) => {
-//     const token = await req.cookies['token']
-//     req.admin = await req.cookies['admin']
-//     if (req.admin) {
-//         next()
-//     } else {
-//         res.render('login', {
-//             layout: 'layouts/main-layout',
-//             title: 'Pelajar NU Wates',
-//             message: 'Anda perlu login dahulu!',
-//             messageClass: 'alert-danger',
-//             admin: null
-//         })
-//     }
-// })
+app.use(async (req, res, next) => {
+    const token = await req.cookies['token']
+    req.admin = await req.cookies['admin']
+    if (req.admin) {
+        next()
+    } else {
+        res.render('login', {
+            layout: 'layouts/main-layout',
+            title: 'Pelajar NU Wates',
+            message: 'Anda perlu login dahulu!',
+            messageClass: 'alert-danger',
+            admin: null
+        })
+    }
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    User.find().sort({ 'data.nama': 1 }).then(users => {
+        res.render('index', {
+            layout: 'layouts/main-layout',
+            title: 'Pelajar NU Wates',
+            admin: req.admin,
+            users
+        })
+    })
 })
 
 // app.post('/filter', async (req, res) => {
